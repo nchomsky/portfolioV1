0aeabf11eaf31d1d47a412bd2ce0fa4e6759dfc3
Noah Chomsky <nbchomp03@gmail.com>
2020-12-15 01:16:29 -0500

    Add app.js to open and close modals

---
+++ app.js
@@ -0,0 +1,22 @@
+const modal = document.querySelector('.popup');
+
+const button = document.querySelector('.btn--projects');
+
+button.addEventListener('click', openModal)
+
+//Listen for click on window
+window.addEventListener('click', closeModal);
+
+// Function to close modal
+function closeModal(e) {
+    console.log(e);
+    if (e.target === modal) {
+        modal.style.visibility = "hidden";
+        modal.style.opacity = 0;
+    }
+}
+
+function openModal() {
+    modal.style.visibility = "";
+    modal.style.opacity = 1;
+}

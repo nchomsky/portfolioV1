c6eaa5bea6a8188c5fd1a2a545946c8571aa9532
Noah Chomsky <nbchomp03@gmail.com>
2020-12-20 00:16:09 -0500

    Add close button for modal

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector('.popup');
 
 const button = document.querySelector('.btn--projects');
+const closeButton = document.querySelector('.close');
 
-button.addEventListener('click', openModal)
+button.addEventListener('click', openModal);
+closeButton.addEventListener('click', closeModal);
 
 //Listen for click on window
 window.addEventListener('click', closeModal);
@@ -10,10 +12,11 @@ window.addEventListener('click', closeModal);
 // Function to close modal
 function closeModal(e) {
     console.log(e);
-    if (e.target === modal) {
+    if (e.target === modal || e.target === closeButton) {
         modal.style.visibility = "hidden";
         modal.style.opacity = 0;
     }
+
 }
 
 function openModal() {
